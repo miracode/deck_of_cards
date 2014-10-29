fcbb1e471a239cddf55f4eb7304893ea8f242d22
miracode <rascatim@gmail.com>
2014-10-29 10:03:49 -0700

    make_deck now creates a brand new sorted deck

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -23,7 +23,8 @@ class Deck(object):
         self.deck.append(card)
 
     def make_deck(self):
-        """Create a deck of 52 cards"""
+        """Create a new deck of 52 cards"""
+        self.deck = []
         ranks = range(2, 11) + ['J', 'Q', 'K', 'A']
         suits = ['hearts', 'diamonds', 'spades', 'clubs']
         for rank in ranks:
